c482effcee16d28e96aadd558625c5b6d16c689a
tdub317 <wong.hsw@gmail.com>
2019-04-29 20:43:46 -0400

    Finished JS and tried to using userHealth as while condition

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let userWins = 0;
 let bossWins = 0;
 let userDmg = 0;
 let bossDmg = 0;
-let play = prompt("Do you want to play?");
+let play = prompt("Do you want to play a game?");
 
 if (play.toUpperCase() === "YES") {
     let userName = prompt("Please name your character");
@@ -15,39 +15,46 @@ if (play.toUpperCase() === "YES") {
         if (bossHealth <= 0) {
             userWins++;
             bossHealth = 10;
-            console.log(`WIN COUNTER: ${userWins}`);
+            // console.log(`WIN COUNTER: ${userWins}`);
         } else if (userHealth <= 0) {
-            console.log(`${userName.toUpperCase()} HAS BEEN DEFEATED. GRANT WINS! GAME OVER.`);
+            console.log(`${userName} HAS BEEN DEFEATED. GRANT WINS! GAME OVER.`);
             break;
         } else if (userWins === 3) {
-            console.log(`${userName.toUpperCase()} WINS! GAME OVER.`);
+            console.log(`${userName} WINS! GAME OVER.`);
             break;
         } else {
             console.log(`${userName} has ${userHealth = userHealth - userDmg} health left.`);
-            console.log(`Grant has ${bossHealth = bossHealth - bossDmg} health left.`);
+            console.log(`The Almighty Grant has ${bossHealth = bossHealth - bossDmg} health left.`);
         }
     }
 }
 
+
+
+// What if we use userHealth for while condition
+// The defeat conditional won't work because the while loop won't run when userHealth is less than or equal to 0
+// If I set the while condition to userHealth less than or equal to 40, it'll run fine
+// If the userHealth is greater than 40, then the while loop won't run
+// Using userWin <= 3 may be better for the solution
+
 // if (play.toUpperCase() === "YES") {
 //     let userName = prompt("Please name your character");
-//     while (userHealth >= 0 && bossHealth >= 0) {
+//     while (userHealth <= 40) {
 //         userDmg = Math.floor(Math.random() * 2) + 1;
 //         bossDmg = Math.floor(Math.random() * 2) + 1;
-//         console.log(`${userName} has ${userHealth = userHealth - userDmg} health left.`);
-//         console.log(`Grant has ${bossHealth = bossHealth - bossDmg} health left.`);
 //         if (bossHealth <= 0) {
-//             wins++;
+//             userWins++;
 //             bossHealth = 10;
-//             console.log(`win counter: ${wins}`);
+//             // console.log(`WIN COUNTER: ${userWins}`);
 //         } else if (userHealth <= 0) {
-//             console.log(`${userName} has been defeated. Grant the Boss wins! Game Over.`);
+//             console.log(`${userName} HAS BEEN DEFEATED. GRANT WINS! GAME OVER.`);
 //             break;
-//         } else if (wins = 3) {
-//             console.log(`${userName} wins! Game Over.`);
+//         } else if (userWins === 3) {
+//             console.log(`${userName} WINS! GAME OVER.`);
 //             break;
 //         } else {
-//             continue;
+//             console.log(`${userName} has ${userHealth = userHealth - userDmg} health left.`);
+//             console.log(`The Almighty Grant has ${bossHealth = bossHealth - bossDmg} health left.`);
 //         }
 //     }
-// } 
+// }
